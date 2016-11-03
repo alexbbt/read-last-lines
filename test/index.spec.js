6fe9ecba6fd7a831e5a02562f5eaeba405a8c78b
Alexander Bell-Towne <alexander@bell-towne.com>
2016-11-03 12:57:48 -0700

    clean up tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,68 +1,43 @@
-var rll = require('../index.js');
-// var mock = require('mock-fs');
+const rll = require('../index.js');
+const expect = require('chai').expect;
 
-var expect = require('chai').expect;
-
-
-// mock({
-// 	'test/': {
-// 		'numbered': '10\n9 9\n8 8\n7 7\n6 6\n5 5\n4 4\n3 3\n2 2\n1 1\n',
-// 		'numbered_no_trailing_new_line': '10\n9 9\n8 8\n7 7\n6 6\n5 5\n4 4\n3 3\n2 2\n1 1'
-// 	}
-// });
 
 describe('#equals', function() {
 	it('return all lines when asked for more than the file has', function() {
 		return rll.read('test/numbered', 15)
 			.then((lines) => {
-				var length = lines.split('\n').length;
-				expect(length).to.equal(10 + 1);
+				var length = lines.trim().split('\n').length;
+				expect(length).to.equal(10);
 			})
-			// .catch(function(error) {
-			// 	console.log(error);
-			// })
 	});
 
 	it('return last line when asked for 1', function() {
 		return rll.read('test/numbered', 1)
 			.then((lines) => {
-				var length = lines.split('\n').length;
-				expect(length).to.equal(1 + 1);
+				var lines = lines.trim().split('\n').length;
+				var length = lines.trim().length;
+				expect(lines).to.equal(1);
+				expect(length).to.equal(3);
 			})
-			// .catch(function(error) {
-			// 	console.log(error);
-			// })
 	});
 
 	it('return last 2 lines when asked for 2', function() {
 		return rll.read('test/numbered', 2)
 			.then((lines) => {
-				var length = lines.split('\n').length;
-				expect(length).to.equal(2 + 1);
+				var length = lines.trim().split('\n').length;
+				expect(length).to.equal(2);
 			})
-			// .catch(function(error) {
-			// 	console.log(error);
-			// })
 	});
 
 	it('return last 2 lines when asked for 2 and missing trailing new line', function() {
 		return rll.read('test/numbered_no_trailing_new_line', 2)
 			.then((lines) => {
-				var length = lines.split('\n').length;
+				var length = lines.trim().split('\n').length;
 				expect(length).to.equal(2);
 			})
-			// .catch(function(error) {
-			// 	console.log(error);
-			// })
 	});
 
 });
-
-			// .then((lines) => {
-			// 	print_results(this.test.title, lines)
-			// 	return lines;
-			// })
-
 
 var print_results = function(title, data) {
 	console.log(JSON.stringify({
