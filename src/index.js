1094901e40e345718ec5d856381e2e82d43c6f92
Alexander Bell-Towne <alexander@bell-towne.com>
2019-03-13 00:43:20 -0400

    linting

--- src/index.js
+++ src/index.js
@@ -34,64 +34,64 @@ module.exports = {
 			};
 
 			fs.exists(input_file_path)
-			.then((exists) => {
-				if (!exists) {
-					throw new Error("file does not exist");
-				}
-
-			}).then(() => {
-				let promises = [];
-
-				// Load file Stats.
-				promises.push(
-					fs.stat(input_file_path)
-						.then(stat => self.stat = stat));
-
-				// Open file for reading.
-				promises.push(
-					fs.open(input_file_path, "r")
-						.then(file => self.file = file));
-
-				return Promise.all(promises);
-			}).then(() => {
-				let chars = 0;
-				let lineCount = 0;
-				let lines = "";
-
-				const do_while_loop = function() {
-					if (lines.length > self.stat.size) {
-						lines = lines.substring(lines.length - self.stat.size);
+				.then((exists) => {
+					if (!exists) {
+						throw new Error("file does not exist");
 					}
 
-					if (lines.length >= self.stat.size || lineCount >= maxLineCount) {
-						if (NEW_LINE_CHARACTERS.includes(lines.substring(0, 1))) {
-							lines = lines.substring(1);
-						}
-						fs.close(self.file);
-						if (encoding === "buffer") {
-							return resolve(Buffer.from(lines, "binary"));
-						}
-						return resolve(Buffer.from(lines, "binary").toString(encoding));
-					}
+				}).then(() => {
+					let promises = [];
 
-					return readPreviousChar(self.stat, self.file, chars)
-						.then((nextCharacter) => {
-							lines = nextCharacter + lines;
-							if (NEW_LINE_CHARACTERS.includes(nextCharacter) && lines.length > 1) {
-								lineCount++;
+					// Load file Stats.
+					promises.push(
+						fs.stat(input_file_path)
+							.then(stat => self.stat = stat));
+
+					// Open file for reading.
+					promises.push(
+						fs.open(input_file_path, "r")
+							.then(file => self.file = file));
+
+					return Promise.all(promises);
+				}).then(() => {
+					let chars = 0;
+					let lineCount = 0;
+					let lines = "";
+
+					const do_while_loop = function() {
+						if (lines.length > self.stat.size) {
+							lines = lines.substring(lines.length - self.stat.size);
+						}
+
+						if (lines.length >= self.stat.size || lineCount >= maxLineCount) {
+							if (NEW_LINE_CHARACTERS.includes(lines.substring(0, 1))) {
+								lines = lines.substring(1);
 							}
-							chars++;
-						})
-						.then(do_while_loop);
-				};
-				return do_while_loop();
+							fs.close(self.file);
+							if (encoding === "buffer") {
+								return resolve(Buffer.from(lines, "binary"));
+							}
+							return resolve(Buffer.from(lines, "binary").toString(encoding));
+						}
 
-			}).catch((reason) => {
-				if (self.file !== null) {
-					fs.close(self.file);
-				}
-				return reject(reason);
-			});
+						return readPreviousChar(self.stat, self.file, chars)
+							.then((nextCharacter) => {
+								lines = nextCharacter + lines;
+								if (NEW_LINE_CHARACTERS.includes(nextCharacter) && lines.length > 1) {
+									lineCount++;
+								}
+								chars++;
+							})
+							.then(do_while_loop);
+					};
+					return do_while_loop();
+
+				}).catch((reason) => {
+					if (self.file !== null) {
+						fs.close(self.file);
+					}
+					return reject(reason);
+				});
 		});
 	},
 };
