ec530f65bdb6b14f36857caf2b51256056ebf46a
Alexander Bell-Towne <alexander@bell-towne.com>
2020-01-13 00:57:37 -0500

    Closes #22 Fix windows newline checking, only need to check for \n and not \r

--- test/index.spec.js
+++ test/index.spec.js
@@ -14,7 +14,7 @@ describe("#read", function() {
 	it("return all lines when asked for more than the file has", function() {
 		return rll.read("test/numbered", 15)
 			.then((lines) => {
-				let length = lines.split(/\n|\r/).length;
+				let length = lines.split(/\n/).length;
 				expect(length).to.equal(10 + 1);
 			});
 	});
@@ -22,7 +22,7 @@ describe("#read", function() {
 	it("return last line when asked for 1", function() {
 		return rll.read("test/numbered", 1)
 			.then((lines) => {
-				let length = lines.split(/\n|\r/).length;
+				let length = lines.split(/\n/).length;
 				let trimmedStringLength = lines.trim().length;
 				expect(length).to.equal(1 + 1);
 				expect(trimmedStringLength).to.equal(3);
@@ -32,7 +32,7 @@ describe("#read", function() {
 	it("return last 2 lines when asked for 2", function() {
 		return rll.read("test/numbered", 2)
 			.then((lines) => {
-				let length = lines.split(/\n|\r/).length;
+				let length = lines.split(/\n/).length;
 				expect(length).to.equal(2 + 1);
 			});
 	});
@@ -40,7 +40,7 @@ describe("#read", function() {
 	it("return last 2 lines when asked for 2 and missing trailing new line", function() {
 		return rll.read("test/numbered_no_trailing_new_line", 2)
 			.then((lines) => {
-				let length = lines.split(/\n|\r/).length;
+				let length = lines.split(/\n/).length;
 				expect(length).to.equal(2);
 			});
 	});
@@ -49,11 +49,11 @@ describe("#read", function() {
 		return assert.isRejected(rll.read("test/non_existant_file_name", 1), "file does not exist");
 	});
 
-	it("should the new line characters used by the file, when using non standard new line characters", function() {
-		return rll.read("test/non_standard_new_lines", 30)
+	it("should work with windows new lines", function() {
+		return rll.read("test/windows_new_lines", 10)
 			.then((lines) => {
-				let length = lines.split(/\n\r/).length;
-				expect(length).to.equal(3 + 1);
+				let length = lines.split(/\r\n/).length;
+				expect(length).to.equal(10 + 1);
 			});
 	});
 
